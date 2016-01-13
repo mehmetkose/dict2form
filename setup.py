b694633ddd9be8e24f2d9d3a229e965c701e5821
mehmetkose <mehmetkose122@gmail.com>
2016-01-13 12:07:37 +0200

    init installer

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='dict2form',
+    version='0.1',
+    packages=[''],
+    url='https://mehmetkose.github.io',
+    license='Apache 2',
+    author='Mehmet Kose',
+    author_email='mehmetkose.py@gmail.com',
+    description='python dictionary object to html5 json form generator.'
+)
