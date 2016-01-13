1a619feda7de8f99a4d5d64f5ee332bb803227e5
mehmetkose <mehmetkose122@gmail.com>
2016-01-13 12:34:28 +0200

    build v0.2

--- build/lib.linux-x86_64-2.7/dict2form.py
+++ build/lib.linux-x86_64-2.7/dict2form.py
@@ -2,7 +2,7 @@
 #-*- coding:utf-8 -*-
 
 
-def generate_inputs(dict_object, hide, name, sub=None):
+def generate_inputs(dict_object, hide, name):
 	body = ""
 	for key in dict_object.keys():
 		if key in hide:
@@ -10,25 +10,19 @@ def generate_inputs(dict_object, hide, name, sub=None):
 		elif isinstance(dict_object[key], str) or isinstance(dict_object[key], unicode):
 			label = "<label for='%s[%s]'>%s</label>" % (name, key, key)
 			input_str = "<input name='%s[%s]' value='%s'><br />" % (name, key, dict_object[key])
-			body += "\n%s\n%s" % (label,input_str)
-			if sub:
-				print("\n\n\n##############")
-				replace_from = "%s[" % (name)
-				replace_to = "%s[%s][" % (name,sub)
-				body.replace(replace_from, replace_to)
-				print(body)
-		elif isinstance(dict_object[key], dict):
-			body += generate_inputs(dict_object[key], hide, name, sub=key)
+			body += "%s\n%s" % (label,input_str)
+		# elif isinstance(dict_object[key], dict):
+		# 	body += generate_inputs(dict_object[key], hide, name, sub=key)
 	return body
 
 
-def dict2form(dict_object, name="object", hide=[], method="get", xsrf=None):
+def dict2form(dict_object, name="object", hide=[], method="get", xsrf=None, submit_name="Submit"):
 	head = "<form enctype='application/json' method='%s'>" % method
-	body = ""
+	body = "<input type='submit' value='%s'>" % (submit_name)
 	bottom = "</form>"
 	if xsrf:
 		body += '<input type="hidden" name="_xsrf" value="%s"/>' % xsrf
-	body += generate_inputs(dict_object, hide, name)
+	body = "%s%s" % (generate_inputs(dict_object, hide, name), body)
 	return "%s\n%s\n%s" % (head, body, bottom)
 
 
--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from distutils.core import setup
 
 setup(
     name='dict2form',
-    version='0.1',
-    packages=[''],
+    version='0.2',
+    packages=[],
     url='https://mehmetkose.github.io',
-    license='Apache 2',
+    license='Apache2',
     author='Mehmet Kose',
     author_email='mehmetkose.py@gmail.com',
     description='python dictionary object to html5 json form generator.'
